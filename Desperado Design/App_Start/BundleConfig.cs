9dcb4f51255c6650f137f9f6a763ced81d8f5cbd
Connie DeCinko <cdecinko@dspdsn.com>
2017-10-10 14:33:00 -0700

    initial check in

---
+++ Desperado Design/App_Start/BundleConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+namespace DesperadoDesign
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/js/jquery.easing.1.3.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/knockout.validation.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/sammy-{version}.js",
+                "~/Scripts/app/common.js",
+                "~/Scripts/app/app.datamodel.js",
+                "~/Scripts/app/app.viewmodel.js",
+                "~/Scripts/app/home.viewmodel.js",
+                "~/Scripts/app/_run.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/owl").Include(
+                "~/js/jquery.waypoints.min.js",
+                "~/js/owl.carousel.min.js",
+                "~/js/main.js"
+                ));
+
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/css/animate.css",
+                 "~/css/icomoon.css",
+                 "~/css/simple-line-icons.css",
+                 "~/css/owl.carousel.min.css",
+                 "~/css/owl.theme.default.min.css",
+                 "~/css/bootstrap.css",
+                 "~/css/brown.css"));
+        }
+    }
+}
